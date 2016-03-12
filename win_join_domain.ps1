d124800b2662a60fb41f1bd2ccbc3a80ad1a0d7f
Nicolas Landais <nicolas.landais@citrix.com>
2016-03-11 22:08:42 -0500

    Adding the win_join_domain custom module

---
+++ win_join_domain.ps1
@@ -0,0 +1,54 @@
+#!powershell
+# This file is part of Ansible
+#
+# Copyright 2016, Citrix Systems, Inc <nicolas.landais@citrix.com>
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# WANT_JSON
+# POWERSHELL_COMMON
+$ErrorActionPreference = "Stop"
+
+$params = Parse-Args $args;
+$result = New-Object PSObject;
+Set-Attr $result "changed" $false;
+
+# Check input parameters
+$user = Get-Attr $params -name "user" -failifempty $true
+$name = Get-Attr $params -name "name" -default $env:computername
+$state = Get-Attr -obj $params -name "state" -default "present"
+$domain = Get-Attr $params -name "domain" -failifempty $true
+$password = Get-Attr $params -name "password" -failifempty $true
+
+# Create PSCredential Object and un/join the domain
+try {
+    $encrypted_password = ConvertTo-SecureString $password -AsPlainText -Force
+    $Credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $user,$encrypted_password
+    if (($state -eq "present") -and ($name -ne $env:computername)) {
+        $output = Add-Computer -NewName $name -Credential $Credential -DomainName $domain -Force
+    } else {
+        $output = Add-Computer -ComputerName $name -Credential $Credential -DomainName $domain -Force
+    }
+    if ($state -eq "absent") {
+        $output = Remove-Computer -ComputerName $name -UnjoinDomainCredential $Credential -Force 
+    }
+    Set-Attr $result "name" $name;
+    Set-Attr $result "domain" $domain
+    Set-Attr $result "output" $output
+    $result.changed = $true
+} catch {
+    $ErrorMessage = $_.Exception.Message
+    Fail-Json $result "Error: $ErrorMessage"
+}
+Exit-Json $result;
---
+++ win_join_domain.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2015, Citrix Systems, Inc <nicolas.landais@citrix.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# this is a windows documentation stub.  actual code lives in the .ps1
+# file of the same name
+
+DOCUMENTATION = '''
+---
+module: win_join_domain
+version_added: "2.1"
+short_description: "Add or remove computer from Active Directory domain"
+description:
+  - This module will add or remove a computer from an Active Directory Domain using a PoSh cmdlet available with PoSh 5.0+
+options:
+  name:
+    description: Name of the computer to un/join to the domain.
+    required: false
+    default: localhost ($ENV:COMPUTERNAME)
+    aliases: []
+  domain:
+    description: Name of the AD domain.
+    required: true
+    default: null
+    aliases: []
+  user: 
+    description: Name of the user account to use to login to the domain.
+    required: true
+    default: null
+    aliases: []
+  password:
+    description:
+      - AD Domain password associated 
+    required: true
+    default: null
+    aliases: []  
+  state:
+    description:
+      - Flag to select whether to join or unjoin the AD domain
+    required: false
+    choices:
+      - present
+      - absent
+    default: present
+    aliases: []
+author: "Nicolas Landais (@nlandais)"
+'''
+
+EXAMPLES = '''
+  # Add machine to the domain
+  win_join_domain:
+    domain: "domain.local"
+    user: "domain\user"
+    password: "qwert123#"
+
+  # Remove machine from the domain
+  win_join_domain:
+    domain: "domain.local"
+    user: "domain\\user"
+    password: "qwert123#"
+    state: absent
+'''
+
+
